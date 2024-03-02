c6adc743547e694d2b87dc5a02cffae436d71668
Aichatou <aichasakho2205@gmail.com>
2024-03-02 22:15:07 +0100

    fist commit

---
+++ src/test/java/org/example/test.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class test {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+
